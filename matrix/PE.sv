7f5c045f34252eb1c4a19519fd7d25c61449af08
Cra2yPierr0t <uchiyama.kazuhide@gmail.com>
2020-12-05 16:51:27 +0900

    [rm] l_d_i_w

--- matrix/PE.sv
+++ matrix/PE.sv
@@ -12,8 +12,6 @@ module PE(
     logic [15:0] buffer_w;
     logic [15:0] buffer;
 
-    logic [15:0] l_d_o_w;
-
     logic [15:0] r_d_o_r;
     
     always_comb begin
@@ -23,14 +21,6 @@ module PE(
             buffer_w = r_d_i;
         end else begin
             buffer_w = buffer + l_d_i * t_d_i;
-        end
-
-        if(reset) begin
-            l_d_o_w = 16'h0000;
-        end else if(read) begin
-            l_d_o_w = buffer;
-        end else begin
-            l_d_o_w = 16'h0000;
         end
 
         r_d_o = r_d_o_r;
