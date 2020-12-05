f563a4a9bccc2144a1fa66b3938a101d57367b27
Cra2yPierr0t <uchiyama.kazuhide@gmail.com>
2020-12-06 04:18:41 +0900

    [fix] spi_slave parametalized

--- io/spi_slave.sv
+++ io/spi_slave.sv
@@ -1,29 +1,38 @@
 module spi_slave #(
     parameter FPGA_CLK  = 12_000_000,
-    parameter SPI_CLK   = 1_000_000
+    parameter SPI_CLK   = 1_000_000,
+    parameter DATA_SIZE = 16
 )(
     spi_if.slv_port spi_port,
     bus_if.mst_port bus_mst_port
 );
 
-    logic   [7:0] mo_data;
-    logic   [2:0] mo_data_index = 3'b000;
+    logic   [DATA_SIZE-1:0] mo_data;
+    logic   [3:0] mo_data_index = 4'b0000;
 
     always_ff @(posedge spi_port.sclk) begin    // read
         mo_data[mo_data_index] <= spi_port.mosi;
-        mo_data_index <= mo_data_index + 3'b001;
+        if(mo_data_index == DATA_SIZE - 1) begin
+            mo_data_index <='0;
+        end else begin
+            mo_data_index <= mo_data_index + '1;
+        end
     end
 
-    logic   [7:0] so_data;
-    logic   [2:0] so_data_index = 3'b000;
+    logic   [DATA_SIZE-1:0] so_data;
+    logic   [3:0] so_data_index = 4'b0000;
 
     always_ff @(negedge spi_port.sclk) begin    // write
         spi_port.miso <= so_data[so_data_index];
-        so_data_index <= so_data_index + 3'b001;
+        if(so_data_index == DATA_SIZE - 1) begin
+            so_data_index <= '0;
+        end else begin
+            so_data_index <= so_data_index + '1;
+        end
     end
 
     always_comb begin
-        bus_mst_port.valid = (mo_data_index == 3'b000);
+        bus_mst_port.valid = (mo_data_index == '0);
         bus_mst_port.data  = mo_data;
     end
 endmodule
