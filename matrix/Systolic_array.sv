bdd92766b150b0b04fa73babf4493c2961edd66a
Cra2yPierr0t <uchiyama.kazuhide@gmail.com>
2020-12-05 15:44:55 +0900

    [add] delay_buffer

--- matrix/Systolic_array.sv
+++ matrix/Systolic_array.sv
@@ -12,7 +12,11 @@ module Systolic_array #(
     logic [15:0] pe_c_w[0:PE_NUMBER-1];
     logic [15:0] pe_r_w[0:PE_NUMBER];
 
-    genvar i;
+    //delay_bufferの数は後で
+    logic [15:0] delay_buffer[1:(PE_NUMBER)*(PE_NUMBER-1)/2];
+    logic [15:0] pe_t_w_delayed[1:PE_NUMBER-1],
+
+    genvar i, j;
     generate
         PE PE(
             .clk    (clk    ),
@@ -25,13 +29,24 @@ module Systolic_array #(
             .r_d_o  (pe_c_w[0])
         );
         for(i = 1; i < PE_NUMBER; i = i + 1) begin : generateArray
+            always_ff @(posedge clk) begin
+                delay_buffer[i*(i+1)/2] <= pe_t_w[i];
+            end
+            if(1 < i) begin
+                for(j = i*(i-1)/2+1; j < i*(i+1)/2; j = j + 1) begin : generate_delay_buffer
+                    delay_buffer[j] <= delay_buffer[j+1];
+                end
+            end
+            always_ff @(posedge clk) begin
+                assign pe_t_w_delayed[i] = delay_buffer[i*(i-1)/2+1];
+            end
             PE PE(
                 .clk    (clk    ),
                 .read   (read   ),
                 .reset  (reset  ),
                 .l_d_i  (pe_c_w[i-1]),
                 .r_d_i  (pe_r_w[i+1]),
-                .t_d_i  (pe_t_w[i]),
+                .t_d_i  (pe_t_w_delayed[i]),
                 .l_d_o  (pe_r_w[i]),
                 .r_d_o  (pe_c_w[i])
             );
