8f2b8891e1077ec7e79c220cfcf7090441e71c4d
cra2ypierr0t <uchiyama.kazuhide@gmail.com>
2020-12-06 17:30:37 +0900

    [fix] main.cpp

--- prog/main.cpp
+++ prog/main.cpp
@@ -3,7 +3,7 @@
 SPI spi(A6, A5, A4);
 DigitalOut cs(A3)
 
-Serial 
+Serial pc(USBTX, USBRX);
 
 int main(){
     spi.format(16, 0);
@@ -14,4 +14,5 @@ int main(){
     spi.write(0x2222);
     buf = spi.write(0x0000)
     cs = 1;
+    pc.printf("buf is 0x%x\n", buf);
 }
