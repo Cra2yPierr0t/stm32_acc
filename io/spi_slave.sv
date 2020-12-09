67a5870b860e43dd895ed1df7bace5cb684c2b8c
Cra2yPierr0t <uchiyama.kazuhide@gmail.com>
2020-12-09 23:24:04 +0900

    [fix] slave system

--- io/spi_slave.sv
+++ io/spi_slave.sv
@@ -15,10 +15,12 @@ module spi_slave #(
     logic   [INDEX_WIDTH-1:0] mo_data_index = '0;
 
     logic   [DATA_SIZE-1:0] so_data = 16'h0000;
-    logic   [INDEX_WIDTH-1:0] so_data_index = DATA_SIZE-1;
+    logic   [INDEX_WIDTH-1:0] so_data_index = 1;
 
     logic [1:0] shift_reg = 2'b00;
     logic [1:0] shift_reg_2 = 2'b00;
+
+    logic a_tempo = 0;
 
     always_ff @(posedge clk) begin
         shift_reg <= {shift_reg[0], spi_port.sclk};
@@ -35,22 +37,23 @@ module spi_slave #(
         end
         
         shift_reg_2 <= {shift_reg_2[0], bus_slv_port.valid};
-        if(shift_reg == 2'b10) begin    // write
+        if(shift_reg == 2'b01) begin    // write
             if(cs == 0) begin
                 //spi_port.miso <= so_data[so_data_index];
                 so_data_index <= so_data_index - 1;
                 if(so_data_index == 0) begin
                     so_data <= '0; 
-                    bus_slv_port.ready = 1;
+                    bus_slv_port.ready <= 1;
                 end else begin
-                    bus_slv_port.ready = 0;
+                    bus_slv_port.ready <= 0;
                 end
             end else begin
-                so_data_index <= DATA_SIZE - 1;
+                //so_data_index <= DATA_SIZE - 1;
+                so_data_index <= 1;
             end
         end else if(shift_reg_2 == 2'b01) begin
             so_data <= bus_slv_port.data;
-            so_data_index <= DATA_SIZE - 1;
+            so_data_index <= 4'hf;
         end else begin
             so_data <= so_data; 
             so_data_index <= so_data_index;
